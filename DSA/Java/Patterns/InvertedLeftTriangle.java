d268d06807bb2ee7cdcb13a689c01d916838c261
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-01-27 22:26:30 +0530

    Create InvertedLeftTriangle.java

---
+++ DSA/Java/Patterns/InvertedLeftTriangle.java
@@ -0,0 +1,30 @@
+package com.shrey.basics.patterns;
+/*
+Enter the value of n :
+5
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+ */
+import java.util.Scanner;
+
+public class InvertedLeftTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n :");
+
+        int n = sc.nextInt();
+        for(int i = n ;i>=1;i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
