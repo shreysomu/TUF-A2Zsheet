602c98ca4e2b1186cfb1424a978c9c9d02a9c9b7
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2026-01-27 22:29:56 +0530

    Create ReverseNumberTriangle.java

---
+++ DSA/Java/Patterns/ReverseNumberTriangle.java
@@ -0,0 +1,30 @@
+package com.shrey.basics.patterns;
+
+/*
+Enter the value of n :
+5
+12345
+1234
+123
+12
+1
+ */
+import java.util.Scanner;
+
+public class ReverseNumberTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n :");
+
+        int n = sc.nextInt();
+        for(int i = n ;i>=1;i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
